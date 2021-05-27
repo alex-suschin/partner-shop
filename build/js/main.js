9e1f1aac9c2883dccf156b60a9d2c32e42caf72a
alex-suschin <alex_suschin@mail.ru>
2021-05-27 16:55:39 +0300

    main-desk

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,49 @@
 $(function() {
 
+
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+
+            daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+            }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+    }
+
+    var deadline = "June 12 2021 00:00:00 GMT+0300";
+    initializeClock('countdown', deadline);
+
+
     $("a.scrollto").click(function() {
         elementClick = $(this).attr("href")
         destination = $(elementClick).offset().top;
@@ -24,10 +68,10 @@ $(function() {
         $('html').removeClass('ov-hidden');
     });
 
-    $('.style-tab').click(function() {
-        $('.style-tabs').find('.active').removeClass('active');
+    $('.catalog-btns a').click(function() {
+        $('.catalog-btns').find('.active').removeClass('active');
         $(this).addClass('active');
-        $('.styles-elems').find('.styles-elem-item').hide();
+        $('.catalog-box').find('.catalog-elem').hide();
         $('#' + $(this).data('switch')).show();
     });
 
@@ -51,6 +95,19 @@ $(function() {
         slidesToScroll: 1,
         speed: 300
     });
+
+    $('.btn-cat').click(function() {
+        $(this).toggleClass('active');
+        $('.menu-cat').toggleClass('active');
+    });
+
+    $(document).click(function(event) {
+        if (!$(event.target).closest(".menu-cat ,.btn-cat").length) {
+            $("body").find(".menu-cat").removeClass("active");
+            $("body").find(".btn-cat").removeClass("active");
+        }
+    });
+
 
 });
 
